6c9728d059a13f9684578c5e17bf53268474d64c
Artem Alchangyan <alchangyan@mail.ru>
2024-09-22 22:21:56 +0400

    added initial view for Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,8 @@
+import { createPortal } from "react-dom";
 import "./Modal.scss";
 
 function Modal() {
-  return <div className="Modal">Modal</div>;
+  return createPortal(<div className="modal-wrapper" />, document.body);
 }
 
 export default Modal;
